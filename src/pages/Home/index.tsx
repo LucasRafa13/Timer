cef6fc9e0fa428e093f31744530879f39141cec3
Lucas <lucasrafael123araujolima@gmail.com>
2023-05-09 13:43:06 -0400

    feat: :sparkles: Table

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,8 +4,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
   StartCountdownButton,
+  TaskInput,
 } from './styles'
 
 export function Home() {
@@ -14,10 +16,27 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput
+            id="task"
+            list="tasks-suggestions"
+            placeholder="Dê um nome para o projeto"
+          />
+
+          <datalist id="tasks-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
